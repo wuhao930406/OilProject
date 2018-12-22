dcee9ea5484cff7f3db2ea46d033462d7efc8e8b
wuhao <wuhao>
2018-12-22 23:58:38 +0800

    uploads

--- src/App.js
+++ src/App.js
@@ -6,20 +6,31 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <div className="header">
+              <ul className="icongroup">
+                  <li>icona</li>
+                  <li>iconb</li>
+              </ul>
+              <div className="logocons">
+                    <p>
+                        Logos
+                    </p>
+              </div>
+          </div>
+          <div className="content">
+              <ul className="mainNav">
+                  <li className="lia">section</li>
+                  <li className="lib">section</li>
+                  <li className="lic">section</li>
+                  <li className="lid">section</li>
+                  <li className="lie">section</li>
+                  <li className="lif">section</li>
+              </ul>
+              <div className="bacs">
+
+              </div>
+              <p class="botbox">Technical support ：CPECC西南分公司      |      版本V2018.12.22</p>
+          </div>
       </div>
     );
   }
