855f4f81cf91b9bab7b0bb20bf09cd1e212be8db
wuhao <wuhao>
2018-12-24 10:24:46 +0800

    10点24分

---
+++ oilproject/precache-manifest.324d187270f98a971faeefd6a42706bd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "985cfc7b304e6c5168c0ffb1d8524df5",
+    "url": "./static/media/topbac.985cfc7b.jpg"
+  },
+  {
+    "revision": "d2c9e680f59bd3c4e875",
+    "url": "./static/css/main.e7830efe.chunk.css"
+  },
+  {
+    "revision": "f715b2e0bea123466b14",
+    "url": "./static/js/1.f715b2e0.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "./static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "d2c9e680f59bd3c4e875",
+    "url": "./static/js/main.d2c9e680.chunk.js"
+  },
+  {
+    "revision": "1d48ab7e1a7617e4acc98d63a04fc543",
+    "url": "./static/media/fog-1.1d48ab7e.png"
+  },
+  {
+    "revision": "b26a32e4b9d3b3567ce2bc90ddc17104",
+    "url": "./static/media/fog-2.b26a32e4.png"
+  },
+  {
+    "revision": "1a05a4ff12163857954b15b99ed51b6f",
+    "url": "./static/media/bactou.1a05a4ff.png"
+  },
+  {
+    "revision": "db41a8d3c04fe2a93cf42943294dc52b",
+    "url": "./static/media/bac.db41a8d3.jpg"
+  },
+  {
+    "revision": "829baf1cf7b0c0f2de6de7d7db6f6e25",
+    "url": "./index.html"
+  }
+];
